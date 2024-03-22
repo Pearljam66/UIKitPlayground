622c1b59d92f5cd3a46bff4ec1e5e1fd5b288cf5
Sarah Clark <sarahireneclark@gmail.com>
2024-03-22 14:42:32 -0500

    Added View code

--- UIKitPlayground.playground/Pages/Practice.xcplaygroundpage/Contents.swift
+++ UIKitPlayground.playground/Pages/Practice.xcplaygroundpage/Contents.swift
@@ -16,19 +16,19 @@ let current = UIDevice.current
 let deviceName = current.systemName
 let deviceVersion = current.systemVersion
 
-print("\(deviceName) \(deviceVersion)")
+//print("\(deviceName) \(deviceVersion)")
 
 let screen = UIScreen.main
 
 let pointsWidth = screen.bounds.size.width
 let pointsHeight = screen.bounds.size.height
-print("Width \(pointsWidth)x Height \(pointsHeight)")
+//print("Width \(pointsWidth)x Height \(pointsHeight)")
 
 let pixelsWidth = screen.nativeBounds.size.width
 let pixelsHeight = screen.nativeBounds.size.height
-print("Width \(pixelsWidth)x Height \(pixelsHeight) ")
+//print("Width \(pixelsWidth)x Height \(pixelsHeight) ")
 
-print("Scale: \(screen.scale)")
+//print("Scale: \(screen.scale)")
 
 // MARK: Windows
 
@@ -37,3 +37,23 @@ print("Scale: \(screen.scale)")
 // isKeyWindow - the window currently in charge of receiving input from the user.
 // makeKeyAndVisible() - This method positions the window in front of any other windows that may exist for the app and makes it visible.
 
+// MARK: View
+// The window is the space where the graphics are displayed, but it does not generate any visible content.
+
+var mainframe = CGRect(x: 0, y: 0, width: 375, height: 667)
+let container = UIView(frame: mainframe)
+
+var color1 = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+container.backgroundColor = color1
+
+let secondContainer = UIView(frame: CGRect(x: 0, y: 0, width: 375, height: 667))
+let color2 = UIColor.systemRed
+container.backgroundColor = color2
+
+let subview1 = UIView(frame: CGRect(x: 20, y: 20, width: 335, height: 300))
+subview1.backgroundColor = UIColor.systemGray4
+let subview2 = UIView(frame: CGRect(x: 20, y: 347, width: 335, height: 300))
+subview2.backgroundColor = UIColor.systemGray4
+
+container.addSubview(subview1)
+container.addSubview(subview2)
