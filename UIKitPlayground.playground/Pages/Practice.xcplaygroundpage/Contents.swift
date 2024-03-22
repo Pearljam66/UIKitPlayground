d1c00d0d5ccca3d4e82fdf7f836baf18663a1f1d
Sarah Clark <sarahireneclark@gmail.com>
2024-03-22 13:07:49 -0500

    Added Application and Device code

--- UIKitPlayground.playground/Pages/Practice.xcplaygroundpage/Contents.swift
+++ UIKitPlayground.playground/Pages/Practice.xcplaygroundpage/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+// MARK: Application
 let app = UIApplication.shared
 
 if app.supportsMultipleScenes {
@@ -8,8 +9,21 @@ if app.supportsMultipleScenes {
     print("Single Window App")
 }
 
+// MARK: Device
 let current = UIDevice.current
 let deviceName = current.systemName
 let deviceVersion = current.systemVersion
 
 print("\(deviceName) \(deviceVersion)")
+
+let screen = UIScreen.main
+
+let pointsWidth = screen.bounds.size.width
+let pointsHeight = screen.bounds.size.height
+print("Width \(pointsWidth)x Height \(pointsHeight)")
+
+let pixelsWidth = screen.nativeBounds.size.width
+let pixelsHeight = screen.nativeBounds.size.height
+print("Width \(pixelsWidth)x Height \(pixelsHeight) ")
+
+print("Scale: \(screen.scale)")
